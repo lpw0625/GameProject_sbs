ac8026545cd39490b6e0093bd092416fa1322f87
Pyeong won Lee <154335742+lpw0625@users.noreply.github.com>
2026-01-15 21:44:07 +0900

    2026_01_15_이중버퍼링

--- Class2th_Time/source.c
+++ Class2th_Time/source.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+#define SIZE 10000
+
+
+
+
 int main()
 {
 #pragma region 파일 입출력
@@ -8,19 +13,56 @@ int main()
 
 	// 첫 번째 매개 변수 (파일의 이름)
 	// 두 번째 매개 변수 (파일의 입출력 모드) 
-	
 
-	FILE* file = fopen("data.txt","w");
+
+	/*FILE* file = fopen("data.txt","w");
 	fputs("Character Information\n", file);
 	fputs("Health : \n", file);
 	fputs("Attack : \n", file);
 	fputs("Defense : \n", file);
 	fclose(file);
 
-	printf("파일쓰기 완료");
+	printf("파일쓰기 완료");*/
+
+#pragma endregion
+
+#pragma region 파일 읽기
+
+	FILE* file = fopen("data.txt", "r");
+
+	int count = 0;
+
+	char chracter = NULL;
+
+
+	while ((chracter = fgetc(file)) != EOF)
+	{
+		count++;
+	}
+
+	printf("count  : %d\n", count);
+
+
+	char buffer[SIZE] = { 0, };
+
+	fread(buffer, sizeof(char), sizeof(buffer), file);
+
+	printf("%s", buffer);
+
+	fclose(file);
+
+
+	// 첫 번째 매개변수 : 읽은 데이터를 저장할 메모리 버퍼의 포인터 함수 
+	// 두 번째 매개변수 : 각 데이터 항목의 크기
+	// 세 번째 매개변수 : 데이터를 읽어올 항목의 수 
+	// 네 번째 매개 변수 : 데이터를 읽어올 파일의 포인터 함수
+
+
 
 #pragma endregion
 
 #pragma endregion
+
+	return 0;
 
 }
